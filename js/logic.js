5825c28a091d3d030201d035a141ecc1f6390a23
interactiveninja <gabrielnadolny2@gmail.com>
2020-08-14 08:47:56 +0200

    Added animation and some buttone logic

--- js/logic.js
+++ js/logic.js
@@ -1,5 +1,14 @@
-$(() =>{
-    $("#darkmode").click(() =>{
-        $("body").toggleClass("dark")
-     })
+$(() => {
+    $("#darkmode").click(() => {
+        if ($("body").hasClass("dark")) {
+            $("#darkmode").text("Dark Mode")
+            $("body").addClass("white")
+            $("body").removeClass("dark")
+        } else {
+            $("#darkmode").text("Light Mode")
+            $("body").addClass("dark")
+            $("body").removeClass("white")
+        }
+        $("darkmode").html("Save")
+    })
 })
